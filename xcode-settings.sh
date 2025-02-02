a5b83441cb884fcf2250a08231887f23ff80a0c2
noah lozevski <noahlozevski@gmail.com>
2025-02-01 16:25:07 -0800

    remove spellcheck xcode setting
    this is highly annoying to see red squiggles on variable names for
    abbreviations

--- xcode-settings.sh
+++ xcode-settings.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-defaults write com.apple.dt.Xcode AutomaticallyCheckSpellingWhileTyping -bool YES
-
 defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool YES
 defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
 
